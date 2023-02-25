963488b2870309ac45c9b0224510c35b1b385cdf
João Vitor Ezequiel <joaoezeki@gmail.com>
2023-02-25 15:38:18 -0300

    eu v2

---
+++ app/src/components/Header.jsx
@@ -0,0 +1,19 @@
+
+
+
+export const Header = () => {
+  return(
+    <div className="Header">
+      <span> &lt;J/V&gt; </span>
+
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Sobre</li>
+          <li>Projetos</li>
+          <li>Contado</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
