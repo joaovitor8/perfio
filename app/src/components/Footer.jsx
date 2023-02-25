c57692ac7cd26cd29aec280f403c8e10d77003d1
João Vitor Ezequiel <joaoezeki@gmail.com>
2023-02-25 19:39:22 -0300

    eu v3

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -1,4 +1,15 @@
 
 
 
-export const Footer = () => {}
+export const Footer = () => {
+  return (
+    <div className="Footer">
+      <span>Feito por João Vitor</span>
+
+      <div>
+        <img src="/linkedin-logo.png" alt="" />
+        <img src="/github-logo.png" alt="" />
+      </div>
+    </div>
+  )
+}
