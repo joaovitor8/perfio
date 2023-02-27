edd83c892dedc74d8b6cd69fd9540b7eaf40467b
João Vitor Ezequiel <joaoezeki@gmail.com>
2023-02-27 13:14:12 -0300

    eu v6

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -5,12 +5,13 @@ import { Sobre } from "./pages/Sobre"
 import { Projetos } from "./pages/Projetos"
 import { Footer } from "./components/Footer"
 import "./App.css"
+import { Perfio } from './perfio/Perfio'
 
 
 export default function App() {
   return (
     <div className="App">
-      <Router>
+      {/* <Router>
         <Header></Header>
               
         <Routes>
@@ -20,7 +21,9 @@ export default function App() {
         </Routes>
 
         <Footer />
-      </Router>
+      </Router> */}
+
+      <Perfio />
     </div>
   )
 }
---
+++ app/src/perfio/Perfio.jsx
@@ -0,0 +1,21 @@
+
+
+export const Perfio = () => {
+  return (
+    <div className="Perfio">
+      <div>
+        <img src="/foto.png" alt="" />
+        <p>João Vitor</p>
+      </div>
+
+      <ul>
+        <li> <a href="https://www.linkedin.com/in/joaovitorezequiel/" target={"_blank"}>Linkedin</a> </li>
+        <li> <a href="https://github.com/joaovitor8" target={"_blank"}>GitHub</a> </li>
+        <li> <a href="https://twitter.com/Joao_Ezeki" target={"_blank"}>Twitter</a> </li>
+      </ul>
+
+      <p>Feito por João Vitor</p>
+
+    </div>
+  )
+}
